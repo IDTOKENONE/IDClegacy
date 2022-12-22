73936c4d0e1477925113454d17cd33ca3210b860
ITO <118643739+IDTOKENONE@users.noreply.github.com>
2022-12-22 17:54:33 +0800

    Update and rename development.js.sample to development.js

---
+++ src/config/environments/development.js
@@ -0,0 +1,13 @@
+module.exports = {
+  networks: {
+    testnet: {
+    lcdURL: 'https://bombay-lcd.terra.dev',
+    chainID: 'bombay-12',
+    factoryContractAddress: 'terra1nt459erudaug8d2vsqjmhv7zc9c9vmamj3esn9',
+    gasPrices: { uusd: 0.15 },
+    allowedPairContracts: [
+      'terra1vmsnmx6q3amlhrkzy8yelefxdv572rw5m4qeec' // TESTA
+    ]
+  }
+  }
+}
