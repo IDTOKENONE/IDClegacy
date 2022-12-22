7f01471475f4ea2e9eb89de888577ecaf5333ab7
ITO <118643739+IDTOKENONE@users.noreply.github.com>
2022-12-22 18:54:58 +0800

    Update networks.js

--- src/config/networks.js
+++ src/config/networks.js
@@ -6,13 +6,13 @@ if(process.env.NODE_ENV !== 'production') {
 }
 
 export const networks = {
-  mainnet: {
+  classic: {
     lcdURL: 'https://lcd.terra.dev',
     chainID: 'columbus-5',
-    factoryContractAddress: '', // Fill in with your project's factory contract
+    factoryContractAddress: 'terra1fnywlw4edny3vw44x04xd67uzkdqluymgreu7g',
     gasPrices: { uusd: 0.15 },
-    allowedPairContracts: [
-      // Fill in with your project's pair contract(s)
+    allowedPairContracts: [ 'terra1jkjpcgn4wywcytpnq0y7wq9jsythz6azmyw5ec'
+      
     ]
   },
   testnet: {
@@ -21,7 +21,7 @@ export const networks = {
     factoryContractAddress: 'terra1nt459erudaug8d2vsqjmhv7zc9c9vmamj3esn9',
     gasPrices: { uusd: 0.15 },
     allowedPairContracts: [
-      'terra1vmsnmx6q3amlhrkzy8yelefxdv572rw5m4qeec' // TESTA
+      'terra1vmsnmx6q3amlhrkzy8yelefxdv572rw5m4qeec'
     ]
   },
   ...envNetworks
