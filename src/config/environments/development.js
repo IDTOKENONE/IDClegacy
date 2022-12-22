cbedae227cb1ca7b4e93e05b5f7af3024ddaf731
ITO <118643739+IDTOKENONE@users.noreply.github.com>
2022-12-22 18:55:59 +0800

    Update development.js

--- src/config/environments/development.js
+++ src/config/environments/development.js
@@ -1,12 +1,12 @@
 module.exports = {
   networks: {
-    testnet: {
-    lcdURL: 'https://pisco-lcd.terra.dev/',
-    chainID: 'pisco-1',
-    factoryContractAddress: 'terra1nt459erudaug8d2vsqjmhv7zc9c9vmamj3esn9',
+    classic: {
+    lcdURL: 'https://lcd.terra.dev',
+    chainID: 'columbus-5',
+    factoryContractAddress: 'terra1fnywlw4edny3vw44x04xd67uzkdqluymgreu7g',
     gasPrices: { uusd: 0.15 },
-    allowedPairContracts: [
-      'terra1vmsnmx6q3amlhrkzy8yelefxdv572rw5m4qeec' // TESTA
+    allowedPairContracts: [ 'terra1jkjpcgn4wywcytpnq0y7wq9jsythz6azmyw5ec'
+      
     ]
   }
   }
