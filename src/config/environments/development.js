b0ede94fe614d59afb45faa653c51bfb34498198
ITO <118643739+IDTOKENONE@users.noreply.github.com>
2022-12-22 18:16:20 +0800

    Update development.js

--- src/config/environments/development.js
+++ src/config/environments/development.js
@@ -1,7 +1,7 @@
 module.exports = {
   networks: {
     testnet: {
-    lcdURL: 'https://bombay.stakesystems.io',
+    lcdURL: 'https://bombay.stakesystems.io/',
     chainID: 'bombay-12',
     factoryContractAddress: 'terra1nt459erudaug8d2vsqjmhv7zc9c9vmamj3esn9',
     gasPrices: { uusd: 0.15 },
