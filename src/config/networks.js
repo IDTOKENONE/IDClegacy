f0fab042e9b70db2261c6b2cf0a883cd3b8036a9
ITO <118643739+IDTOKENONE@users.noreply.github.com>
2022-12-22 18:15:48 +0800

    Update networks.js

--- src/config/networks.js
+++ src/config/networks.js
@@ -16,7 +16,7 @@ export const networks = {
     ]
   },
   testnet: {
-    lcdURL: 'https://bombay.stakesystems.io',
+    lcdURL: 'https://bombay.stakesystems.io/',
     chainID: 'bombay-12',
     factoryContractAddress: 'terra1nt459erudaug8d2vsqjmhv7zc9c9vmamj3esn9',
     gasPrices: { uusd: 0.15 },
