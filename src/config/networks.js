f82dbde204a9171842edbc49045af646c3721792
ITO <118643739+IDTOKENONE@users.noreply.github.com>
2022-12-22 18:22:13 +0800

    Update networks.js

--- src/config/networks.js
+++ src/config/networks.js
@@ -7,8 +7,8 @@ if(process.env.NODE_ENV !== 'production') {
 
 export const networks = {
   mainnet: {
-    lcdURL: 'https://lcd.terra.dev',
-    chainID: 'columbus-5',
+    lcdURL: 'https://pisco-lcd.terra.dev/',
+    chainID: 'pisco-1',
     factoryContractAddress: '', // Fill in with your project's factory contract
     gasPrices: { uusd: 0.15 },
     allowedPairContracts: [
