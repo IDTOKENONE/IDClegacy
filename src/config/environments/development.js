d24b9e97138b335c33b06e0d7ec033753cb54d93
BAZ <118599888+BAZustc@users.noreply.github.com>
2022-12-22 23:41:15 +0800

    Update development.js

--- src/config/environments/development.js
+++ src/config/environments/development.js
@@ -3,9 +3,9 @@ module.exports = {
     classic: {
     lcdURL: 'https://lcd.terra.dev',
     chainID: 'columbus-5',
-    factoryContractAddress: 'terra1fnywlw4edny3vw44x04xd67uzkdqluymgreu7g',
+    factoryContractAddress: 'terra1jkndu9w5attpz09ut02sgey5dd3e8sq5watzm0',
     gasPrices: { uusd: 0.15 },
-    allowedPairContracts: [ 'terra1sz988qp6vma3j0xj5w7fsskqcuc8kjn0mmtcqc'
+    allowedPairContracts: [ 'terra1wqvhrrzkcn7dv2rkd5gdq2prawm64g7lnasy47'
       
     ]
   }
